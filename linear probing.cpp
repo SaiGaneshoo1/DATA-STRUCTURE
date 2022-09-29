7a38ca7086a2970a80a59868a36628770f6bcbb7
SaiGaneshoo1 <113955895+SaiGaneshoo1@users.noreply.github.com>
2022-09-29 22:02:06 +0530

    Update linear probing.cpp

--- linear probing.cpp
+++ linear probing.cpp
@@ -1,191 +1,83 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
- 
-struct item 
+#define TABLE_SIZE 10
+
+int h[TABLE_SIZE]={NULL};
+
+void insert()
 {
-    int key;
-    int value;
-};
- 
-struct hashtable_item 
-{
- 
-    int flag;
- 
-    struct item *data;
- 
-};
- 
-struct hashtable_item *array;
-int size = 0;
-int max = 10;
- 
-void init_array()
-{
-    int i;
-    for (i = 0; i < max; i++) 
+
+ int key,index,i,flag=0,hkey;
+ printf("\nenter a value to insert into hash table\n");
+ scanf("%d",&key);
+ hkey=key%TABLE_SIZE;
+ for(i=0;i<TABLE_SIZE;i++)
     {
-	array[i].flag = 0;
-	array[i].data = NULL;
+
+     index=(hkey+i)%TABLE_SIZE;
+
+     if(h[index] == NULL)
+     {
+        h[index]=key;
+         break;
+     }
+
     }
+
+    if(i == TABLE_SIZE)
+
+     printf("\nelement cannot be inserted\n");
 }
- 
-int hashcode(int key)
+void search()
 {
-    return (key % max);
-}
- 
-void insert(int key, int value)
-{
-    int index = hashcode(key);
-    int i = index;
- 
-    struct item *new_item = (struct item*) malloc(sizeof(struct item));
-    new_item->key = key;
-    new_item->value = value;
- 
-    while (array[i].flag == 1) 
+
+ int key,index,i,flag=0,hkey;
+ printf("\nenter search element\n");
+ scanf("%d",&key);
+ hkey=key%TABLE_SIZE;
+ for(i=0;i<TABLE_SIZE; i++)
+ {
+    index=(hkey+i)%TABLE_SIZE;
+    if(h[index]==key)
     {
- 
-	if (array[i].data->key == key) 
-        {
- 
-		printf("\n Key already exists, hence updating its value \n");
-		array[i].data->value = value;
-		return;
- 
-	}
- 
-	i = (i + 1) % max;
-	if (i == index) 
-        {
-		printf("\n Hash table is full, cannot insert any more item \n");
-		return;
-	}
- 
+      printf("value is found at index %d",index);
+      break;
     }
- 
-    array[i].flag = 1;
-    array[i].data = new_item;
-    size++;
-    printf("\n Key (%d) has been inserted \n", key);
- 
-} 
- 
- 
-void remove_element(int key)
-{
-    int index = hashcode(key);
-    int  i = index;
- 
-    while (array[i].flag != 0) 
-    {
- 
-	if (array[i].flag == 1  &&  array[i].data->key == key ) 
-        {
- 
-		array[i].flag =  2;
-		array[i].data = NULL;
-		size--;
-		printf("\n Key (%d) has been removed \n", key);
-		return;
- 
-	}
-	i = (i + 1) % max;
-	if (i == index)
-        {
-		break;
-	}
- 
-    }
- 
-    printf("\n This key does not exist \n");
- 
+  }
+  if(i == TABLE_SIZE)
+    printf("\n value is not found\n");
 }
- 
 void display()
 {
-    int i;
-    for (i = 0; i < max; i++)
+
+  int i;
+
+  printf("\nelements in the hash table are \n");
+
+  for(i=0;i< TABLE_SIZE; i++)
+
+  printf("\nat index %d \t value =  %d",i,h[i]);
+
+}
+main()
+{
+    int opt,i;
+    while(1)
     {
-	struct item *current = (struct item*) array[i].data;
- 
-	if (current == NULL) 
+        printf("\nPress 1. Insert\t 2. Display \t3. Search \t4.Exit \n");
+        scanf("%d",&opt);
+        switch(opt)
         {
-	    printf("\n Array[%d] has no elements \n", i);
-	}
-	else
-        {
-	    printf("\n Array[%d] has elements -: \n  %d (key) and %d(value) ", i, current->key, current->value);
-	}
+            case 1:
+                insert();
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                search();
+                break;
+            case 4:exit(0);
+        }
     }
- 
-}
- 
-int size_of_hashtable()
-{
-    return size;
-}
- 
-int main()
-{
-	int choice, key, value, n, c;
- 
-	array = (struct hashtable_item*) malloc(max * sizeof(struct hashtable_item*));
-	init_array();
- 
-	do {
-		printf("Implementation of Hash Table in C with Linear Probing \n\n");
-		printf("MENU-: \n1.Inserting item in the Hashtable" 
-                              "\n2.Removing item from the Hashtable" 
-                              "\n3.Check the size of Hashtable"
-                              "\n4.Display Hashtable"
-		       "\n\n Please enter your choice-:");
- 
-		scanf("%d", &choice);
- 
-		switch(choice) 
-                {
- 
-		case 1:
- 
-		      printf("Inserting element in Hashtable\n");
-		      printf("Enter key and value-:\t");
-		      scanf("%d %d", &key, &value);
-		      insert(key, value);
- 
-		      break;
- 
-		case 2:
- 
-		      printf("Deleting in Hashtable \n Enter the key to delete-:");
-		      scanf("%d", &key);
-		      remove_element(key);
- 
-		      break;
- 
-		case 3:
- 
-		      n = size_of_hashtable();
-		      printf("Size of Hashtable is-:%d\n", n);
- 
-		      break;
- 
-		case 4:
- 
-		      display();
- 
-		      break;
- 
-		default:
- 
-		       printf("Wrong Input\n");
- 
-		}
- 
-		printf("\n Do you want to continue-:(press 1 for yes)\t");
-		scanf("%d", &c);
- 
-	}while(c == 1);
- 
 }
